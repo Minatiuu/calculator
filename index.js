e67e02f87245aeb54c58dca26cb4c30c9b7266e8
DevAles <juniomalabal@gmail.com>
2021-12-04 20:51:14 -0300

    js

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ function SendInput(id)
 {
     const input_register = document.getElementById(id).innerHTML
     document.getElementById("display").innerHTML = input_register
-    if (document.getElementById("display").indexOf("+"))
-    {
-        console.log("This is a sum")
-    }
 }
