bbb5a91431a4f3991be405557aedf82f90d4dee1
DevAles <juniomalabal@gmail.com>
2021-12-03 23:51:25 -0300

    js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 "use strict"
-let number_input;
-let number_input2;
 
 function SendNum(id)
 {
     const number_register = document.getElementById(id).innerHTML
-    document.getElementById("number_1").innerHTML = number_register
+    document.getElementById("display").innerHTML = number_register
 }
