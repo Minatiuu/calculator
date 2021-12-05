b785d3864f3a7defeede90e5243f974b900e2ee2
DevAles <juniomalabal@gmail.com>
2021-12-05 10:44:20 -0300

    js

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ function SendInput(id)
     }
     else
     {
-        const doc = document.getElementById("display3").innerHTML
-        const input_register = document.getElementById(id).innerHTML
-        document.getElementById("display3").innerHTML = doc + input_register
+        state_display = 1
     }
     
 }
@@ -35,6 +33,20 @@ function SymbolAdd(id)
     else
     {
         state_display = 1
+    }
+}
+
+function SendInput2(id)
+{
+    if (state_display == 0)
+    {
+        state_display = 1
+    }
+    else
+    {
+        const doc = document.getElementById("display3").innerHTML
+        const input_register = document.getElementById(id).innerHTML
+        document.getElementById("display3").innerHTML = doc + input_register
     }
 }
 
