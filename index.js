b8aa5d24491b31c84a28b8a1fa138bc6d778835e
DevAles <juniomalabal@gmail.com>
2021-12-04 21:18:41 -0300

    js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 function SendInput(id)
 {
+    const doc = document.getElementById("number_1").innerHTML
     const input_register = document.getElementById(id).innerHTML
-    document.getElementById("number_1").innerHTML = input_register
-
+    document.getElementById("number_1").innerHTML = doc + input_register
 }
 
 function Equal()
