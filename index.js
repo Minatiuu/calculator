4193c88d68b7c3ba7b66514bcb2874492f5a707c
DevAles <juniomalabal@gmail.com>
2021-12-06 21:20:40 -0300

    Atualização de Logs

--- index.js
+++ index.js
@@ -10,7 +10,31 @@ function SendInput(id)
 function Equal()
 {
     const doc = document.getElementById("display").innerHTML
-    return document.getElementById("display").innerHTML = eval(doc)
+    if (document.getElementById("display").innerHTML != "")
+    {
+        try {
+            document.getElementById("display").innerHTML = eval(doc)
+        }
+        catch(err)
+        {
+            if (err.name = "SyntaxError")
+            {
+                document.getElementById("data_message").innerHTML = ""
+                document.getElementById("data_message").innerHTML = "Sintaxe Mal Formada"
+            }
+            if (err.name = "TypeError")
+            {
+                document.getElementById("data_message").innerHTML = ""
+                document.getElementById("data_message").innerHTML = "Sintaxe Mal Formada"
+            }
+        }
+    }
+    else
+    {
+        return document.getElementById("display").innerHTML = ""
+    }
+
+    
 }
 
 function ClearAll()
