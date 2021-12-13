dd82d5f47371b0f29c34cec053c40a131b0a7bf9
DevAles <juniomalabal@gmail.com>
2021-12-12 21:42:20 -0300

    Correção de Bugs

--- script/index.js
+++ script/index.js
@@ -1,82 +1,107 @@
 "use strict"
 $(document).ready(function() {
-$.getJSON('../data/geral.json', function(data){
-
-$('.numbers_row > button').click(function(){
-    const doc = $('#display').html()
-    const input_register = $('#' + this.id).html()
-    $('#display').html(doc + input_register)
-});
-
-$('.flex_divisor > button').click(function(){
-    const doc = $('#display').html()
-    const input_register = $('#' + this.id).html()
-    $('#display').html(doc + input_register)
-});
-
-$(document).keydown(function(){
-    const doc = $('#display').html()
-    const key = event.key
-    const accepted_keys = ["0","1","2","3","4","5","6","7","8","9","-","+","=","(",")","*","%"]
-    if (accepted_keys.includes(key))
-    {
-        $('#display').html(doc + key)
-    }
-    console.log(key)
-});
-
-$('#number_equal').click(function(){
-    const doc = $('#display').html()
-
-    if (document.getElementById("display").innerHTML != "")
-    {
-        try {
-            if (eval(doc) == Infinity)
+    $.getJSON('../data/geral.json', function(data){
+        $('.numbers_row > button').click(function(){
+            const doc = document.getElementById("display").innerHTML
+            const input_register = document.getElementById(this.id).innerHTML
+            if (input_register != "=")
             {
-                $('#display').html("")
-                document.getElementById("data_message").style.color = "red"
-                $('#data_message').html("Valor Máximo Excedido")
+                document.getElementById("display").innerHTML = doc + input_register
             }
             else
             {
-                $('#display').html(eval(doc))
+                Equal()
             }
-        }
-        catch(err)
+        });
+        $('.flex_divisor > button').click(function(){
+            const doc = document.getElementById("display").innerHTML
+            const input_register = document.getElementById(this.id).innerHTML
+            document.getElementById("display").innerHTML = doc + input_register
+        });
+        
+        function SendButton()
         {
-
-            if (err.name = "SyntaxError" || "TypeError")
+            const doc = document.getElementById("display").innerHTML
+            const key = event.key
+            const accepted_keys = ["0","1","2","3","4","5","6","7","8","9","-","+","=","(",")","*","%"]
+            if (accepted_keys.includes(key))
             {
-                document.getElementById("data_message").style.color = "red"
-                $('#data_message').html("Sintaxe Mal Formada")
-                console.log(err.name)
+                document.getElementById("display").innerHTML = doc + key
             }
-            else
-            {
-                $('#data_message').html("")
-                document.getElementById("data_message").style.color = "red"
-                $('#data_message').html("Erro Desconhecido")
-                console.log(err.name)
-            }
-
+            console.log(key)
         }
         
-    }
-    else
-    {
-        $('#display').html("")
-    }
-});
-
-$('#number_clear').click(function(){
-    const doc = $('#display').html()
-    $('#display').html(doc.slice(0,-1))
-});
-
-$('#number_clearall').click(function(){
-    $('#data_message').html("")
-    $('#display').html("")
-});
-
-});
+        function Equal()
+        {
+            const doc = document.getElementById("display").innerHTML
+        
+            if (document.getElementById("display").innerHTML != "")
+            {
+                try {
+                    if (eval(doc) == Infinity)
+                    {
+                        document.getElementById("data_message").innerHTML = ""
+                        document.getElementById("data_message").style.color = "red"
+                        document.getElementById("data_message").innerHTML = "Valor Máximo Excedido"
+                    }
+                    else
+                    {
+                        document.getElementById("display").innerHTML = eval(doc)
+                    }
+                }
+                catch(err)
+                {
+        
+                    if (err.name = "SyntaxError" || "TypeError")
+                    {
+                        document.getElementById("data_message").style.color = "red"
+                        document.getElementById("data_message").innerHTML = "Sintaxe Mal Formada"
+                        console.log(err.name)
+                    }
+                    else
+                    {
+                        document.getElementById("data_message").innerHTML = ""
+                        document.getElementById("data_message").style.color = "red"
+                        document.getElementById("data_message").innerHTML = "Erro Desconhecido"
+                        console.log(err.name)
+                    }
+        
+                }
+            }
+            else
+            {
+                return document.getElementById("display").innerHTML = ""
+            }
+        
+        
+        }
+        
+        function Clear()
+        {
+            const doc = document.getElementById("display").innerHTML
+            document.getElementById("display").innerHTML = doc.slice(0,-1)
+        }
+        
+        function ClearAll()
+        {
+            document.getElementById("data_message").innerHTML = ""
+            document.getElementById("display").innerHTML = ""
+        }
+        
+        $(document).keydown(function(){
+            SendButton()
+        });
+        
+        $('#number_equal').click(function(){
+            Equal()
+        });
+        
+        $('#number_clear').click(function(){
+            Clear()
+        });
+        
+        $('#number_clearall').click(function(){
+            ClearAll()
+        });        
+    });
 });
